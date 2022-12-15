fe4ea91c69083dae935e89a44603d2ead9cef4ab
Krishna Bhogaonker <cyclotomiq@gmail.com>
2022-12-15 17:32:14 -0500

    trying to download files into local directory for testing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,12 @@
 using JuliaGendUniv
+using URLDownload
 using Test
 
+# pwd() = "/home/runner/work/JuliaGendUniv.jl/JuliaGendUniv.jl/test"
 @show pwd()
+urldownload("https://github.com/university-gender-evolution/JuliaGendUniv.jl/releases/download/0.1.0/michigan1979to2009_wGender.dta")
 
 @testset "JuliaGendUniv.jl" begin
     # Write your tests here.
+    @test isfile("michigan1979to2009_wGender.dta")
 end
