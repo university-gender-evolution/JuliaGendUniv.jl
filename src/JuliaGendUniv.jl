19671287d98ad842487336da8f5eb901f1a41675
Krishna Bhogaonker <cyclotomiq@gmail.com>
2022-12-14 22:00:51 -0500

    started to build out src folder with JuliaGendUniv.jl file, and types.

--- src/JuliaGendUniv.jl
+++ src/JuliaGendUniv.jl
@@ -1,5 +1,35 @@
 module JuliaGendUniv
 
-# Write your package code here.
+
+using DataFrames
+using CSV
+using FreqTables
+using Plots
+using StatsPlots
+using StatFiles
+using Pipe, StatsBase, Chain
+using DataFramesMeta
+using Clustering
+using MultivariateStats
+using Debugger
+using RCall
+using Distances
+using Interpolations
+using Zygote
+using KernelDensity
+using PDFmerger
+using Dates
+using Optimization
+using DiffEqFlux
+using ComponentArrays
+using Combinatorics
+
+include("PreprocessingTypes.jl");
+include("PrepareUMData.jl");
+
+
+
+
+
 
 end
