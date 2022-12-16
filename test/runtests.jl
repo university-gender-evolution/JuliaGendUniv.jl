40c18dafaef472cbeca2ce821156637cf30070a0
Krishna Bhogaonker <cyclotomiq@gmail.com>
2022-12-16 12:18:12 -0500

    cleaning up build process and CI.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,6 @@
 using JuliaGendUniv
 using Test
 
-# pwd() = "/home/runner/work/JuliaGendUniv.jl/JuliaGendUniv.jl/test"
-@show pwd()
-#urldownload("https://github.com/university-gender-evolution/JuliaGendUniv.jl/releases/download/0.1.0/michigan1979to2009_wGender.dta")
 
 @testset "JuliaGendUniv.jl" begin
     # Write your tests here.
