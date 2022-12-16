be45c152ee26f35a9fd5c1c3960a0cefccc0db5f
Krishna Bhogaonker <cyclotomiq@gmail.com>
2022-12-16 16:41:04 -0500

    testing to debug precompliation issue.

--- src/JuliaGendUniv.jl
+++ src/JuliaGendUniv.jl
@@ -2,29 +2,29 @@ module JuliaGendUniv
 
 
 using DataFrames
-using CSV
-using FreqTables
-using Plots
-using StatsPlots
-using StatFiles
-using Pipe, StatsBase, Chain
-using DataFramesMeta
-using Clustering
-using MultivariateStats
-using RCall
-using Distances
-using Interpolations
-using Zygote
-using KernelDensity
-using PDFmerger
-using Dates
-using Optimization
-using DiffEqFlux
-using ComponentArrays
-using Combinatorics
+# using CSV
+# using FreqTables
+# using Plots
+# using StatsPlots
+# using StatFiles
+# using Pipe, StatsBase, Chain
+# using DataFramesMeta
+# using Clustering
+# using MultivariateStats
+# using RCall
+# using Distances
+# using Interpolations
+# using Zygote
+# using KernelDensity
+# using PDFmerger
+# using Dates
+# using Optimization
+# using DiffEqFlux
+# using ComponentArrays
+# using Combinatorics
 
-include("PreprocessingTypes.jl");
-#include("PrepareUMData.jl");
+# include("PreprocessingTypes.jl");
+# #include("PrepareUMData.jl");
 
 
 
