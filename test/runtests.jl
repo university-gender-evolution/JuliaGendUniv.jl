f987d3a94914c98dc8829d74814e8a01ee8a9b3c
Krishna Bhogaonker <cyclotomiq@gmail.com>
2022-12-16 00:10:41 -0500

    updated test to remove urldownload

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,9 @@ using Test
 
 # pwd() = "/home/runner/work/JuliaGendUniv.jl/JuliaGendUniv.jl/test"
 @show pwd()
-urldownload("https://github.com/university-gender-evolution/JuliaGendUniv.jl/releases/download/0.1.0/michigan1979to2009_wGender.dta")
+#urldownload("https://github.com/university-gender-evolution/JuliaGendUniv.jl/releases/download/0.1.0/michigan1979to2009_wGender.dta")
 
 @testset "JuliaGendUniv.jl" begin
     # Write your tests here.
-    @test isfile("michigan1979to2009_wGender.dta")
+    @test isfile("../michigan1979to2009_wGender.dta")
 end
