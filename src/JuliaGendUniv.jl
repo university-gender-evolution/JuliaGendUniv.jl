aee4014f14df2fc41cb20de86797e3e40aecd54e
Krishna Bhogaonker <cyclotomiq@gmail.com>
2022-12-16 17:07:49 -0500

    re-enabled some package imports to see if precompile issue is affected.

--- src/JuliaGendUniv.jl
+++ src/JuliaGendUniv.jl
@@ -2,20 +2,20 @@ module JuliaGendUniv
 
 
 using DataFrames
-# using CSV
-# using FreqTables
-# using Plots
-# using StatsPlots
-# using StatFiles
-# using Pipe, StatsBase, Chain
-# using DataFramesMeta
-# using Clustering
-# using MultivariateStats
+using CSV
+using FreqTables
+using Plots
+using StatsPlots
+using StatFiles
+using Pipe, StatsBase, Chain
+using DataFramesMeta
+using Clustering
+using MultivariateStats
 # using RCall
-# using Distances
-# using Interpolations
-# using Zygote
-# using KernelDensity
+using Distances
+using Interpolations
+using Zygote
+using KernelDensity
 # using PDFmerger
 # using Dates
 # using Optimization
