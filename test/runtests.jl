47f92261c6b54ff6b054871f6cfe80bbb2ace349
Krishna Bhogaonker <cyclotomiq@gmail.com>
2022-12-17 15:32:33 -0500

    updated title of testset.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using JuliaGendUniv
 using Test
 
 
-@testset "JuliaGendUniv.jl" begin
+@testset "JuliaGendUniv environment setup" begin
     # Write your tests here.
     @test 1 + 1 == 2
     @test isFile("michigan1979to2009_wGender.dta")
