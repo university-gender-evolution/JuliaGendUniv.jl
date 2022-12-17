709c4c9061772061c51722b75a38f91f61d87723
Krishna Bhogaonker <cyclotomiq@gmail.com>
2022-12-17 16:11:10 -0500

    Add test file existence (#26)
    * added test for file existence and updated gitignore to ignore datafile.
    
    * updated title of testset.
    
    * updated datafile testcase and CI yml cleanup.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,8 @@ using JuliaGendUniv
 using Test
 
 
-@testset "JuliaGendUniv.jl" begin
+@testset "JuliaGendUniv environment setup" begin
     # Write your tests here.
     @test 1 + 1 == 2
+    @test isfile("michigan1979to2009_wGender.dta")
 end
