8ea2d6f680d1f0cca1833c818030e5b5aeef8aef
Krishna Bhogaonker <cyclotomiq@gmail.com>
2022-12-28 23:53:19 -0500

    enabled basic filtering and it seems to work.

--- src/PrepareUMData.jl
+++ src/PrepareUMData.jl
@@ -18,6 +18,27 @@ function _setup_data(file_path::String, ::UM)
 end;
 
 
+function _get_departments_with_target_start_year!(univdata::GendUnivData, ::UM)
+    outdf = subset(univdata._raw_df, :year => ByRow(==(univdata.first_year)))
+    univdata.department_names = unique(outdf.orgname)
+    univdata.processed_df = @rsubset(univdata._raw_df, :orgname ∈ univdata.department_names)
+end;
+
+
+function _get_departments_with_professors!(univdata::GendUnivData, ::UM)
+    univdata.processed_df = filter(:jobdes => contains("PROF"), univdata.processed_df)
+    univdata.department_names = unique(univdata.processed_df.orgname)
+end;
+
+
+function _get_departments_with_sufficient_years!(univdata::GendUnivData, ::UM)
+    outdf = unique(univdata.processed_df, [:orgname, :year])
+    gdf = groupby(outdf, [:orgname])
+    cdf = combine(gdf, nrow )
+    res = filter(:nrow => >=(univdata.num_years), cdf)
+    univdata.processed_df = subset(univdata.processed_df, :orgname => x -> x .∈ [res.orgname])
+    univdata.department_names = res.orgname
+end;
 function preprocess_data(file_path::String, 
                         first_year::Integer, 
                         num_years::Integer, 
@@ -25,11 +46,11 @@ function preprocess_data(file_path::String,
                         audit_config::AbstractDataChecks=NoAudit())
 
     univ_data = _load_univ_data(file_path, config)
-    # univ_data.first_year = first_year
-    # univ_data.num_years = num_years
-    # _get_departments_with_target_start_year!(univ_data, config)
-    # _get_departments_with_professors!(univ_data, config)
-    # _get_departments_with_sufficient_years!(univ_data, config) 
+    univ_data.first_year = first_year
+    univ_data.num_years = num_years
+    _get_departments_with_target_start_year!(univ_data, config)
+    _get_departments_with_professors!(univ_data, config)
+    _get_departments_with_sufficient_years!(univ_data, config) 
     # _process_each_dept!(univ_data, config, audit_config)
     # _postprocess_data_arrays!(univ_data, config)
     return univ_data
