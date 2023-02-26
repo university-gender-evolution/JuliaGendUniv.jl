907b6d37f5079ddeb78729cfae0641d151123fbc
Krishna Bhogaonker <cyclotomiq@gmail.com>
2023-02-26 15:26:19 -0500

    added optimization types and started to write up API.

--- src/JuliaGendUniv.jl
+++ src/JuliaGendUniv.jl
@@ -24,12 +24,13 @@ using ComponentArrays
 using Combinatorics
 
 include("PreprocessingTypes.jl");
+include("OptimizationTypes.jl");
 include("UMPrepareData.jl");
 include("UMDepartmentProcessing.jl")
-
+include("OptimizeDept.jl")
 
 export preprocess_data, UM, DataAudit, NoAudit, preprocess_dept_train_test_split,
-        get_department_data
+        get_department_data, optimize_model
 
 
 
--- src/OptimizationTypes.jl
+++ src/OptimizationTypes.jl
@@ -6,8 +6,23 @@ abstract type AbstractGendUnivModel end;
 abstract type AbstractModelOptSettings end;
 
 
-mutable struct 
-    raw_df::DataFrame
-    processed_data::DataFrame
+mutable struct GendUnivOdeModel <: AbstractGendUnivModel
+    processed_data::UMDeptData
+    boot_spline_data::DataFrame
     first_year::Integer
     num_years::Integer
+    timespan::Tuple{Integer, Integer}
+    initial_values::DataFrame # change this. 
+    params::DataFrame # change this
+end;
+
+function GendUnivOdeModel(dept::UMDeptData, 
+                            modelfunc::Function, 
+                            first_year, 
+                            num_years,
+                            initial_values,
+                            params)
+
+
+
+end;
